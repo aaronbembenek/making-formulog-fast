74b97e60c37f9a63f302489ccee53cc057154e4f
Aaron Bembenek <aaron.bembenek@gmail.com>
2024-07-03 14:32:09 +1000

    Draft script for running all experiments.

--- scripts/eval_compiler.sh
+++ scripts/eval_compiler.sh
@@ -9,17 +9,112 @@ fi
 script_dir=$(dirname -- "$(readlink -f -- "$0")")
 cd $script_dir/..
 
+# The follow options are supported for the `--modes` argument`:
+#
+#   `interpret`: use Formulog interpreter in semi-naive mode
+#
+#   `interpret-reorder`: reorder each rule body so delta atom is first, and then
+#   use Formulog interpreter in semi-naive mode
+#
+#   `interpret-unbatched`: use Formulog interpreter in eager evaluation mode
+#
+#   `compile`: use Formulog compiler to generate code performing semi-naive
+#   evaluation
+#
+#   `compile-reorder`: reorder each rule body so delta atom is first, and then
+#   use Formulog compiler to generate code performing semi-naive evaluation
+#
+#   `compile-unbatched`: use Formulog compiler to generate code performing eager
+#   evaluation
+#
+#   `klee`: use KLEE (symex only)
+#
+#   `cbmc`: use CBMC (symex only)
+#
+#   `scuba`: use the reference Scuba implementation (scuba only)
+
+# Modify these variables to shorten the length of the experiments
+ntrials=10
+timeout=1800 # seconds
+
+## In what follows, comment out modes and benchmarks you do not want to run
+
+## DMINOR
+
+dminor_modes=(
+    interpret-reorder
+    interpret-unbatched
+    compile-reorder
+    compile-unbatched
+)
+
+dminor_bms=(
+    benchmarks/dminor/bms/all-1
+    benchmarks/dminor/bms/all-10
+    benchmarks/dminor/bms/all-100
+)
+
 python3 -u scripts/bench.py --output-dir "$1" \
-    --modes interpret-reorder interpret-unbatched compile-reorder compile-unbatched \
-    --ntrials 10 -j 40 --smt-solver-modes push-pop \
-    --benchmark-dirs benchmarks/dminor/bms/* --record-work --small-tasks
+    --modes "${dminor_modes[@]}" \
+    --ntrials $ntrials -j 40 --smt-solver-modes push-pop \
+    --record-work --small-tasks --timeout $timeout \
+    --benchmark-dirs "${dminor_bms[@]}"
+
+## SYMEX
+
+symex_modes=(
+    interpret-reorder
+    interpret-unbatched
+    compile-reorder
+    compile-unbatched
+    cbmc
+    klee
+)
+
+symex_bms=(
+    benchmarks/symex/bms/interp-5
+    benchmarks/symex/bms/interp-6
+    benchmarks/symex/bms/numbrix-sat
+    benchmarks/symex/bms/numbrix-unsat
+    benchmarks/symex/bms/prioqueue-5
+    benchmarks/symex/bms/prioqueue-6
+    benchmarks/symex/bms/shuffle-4
+    benchmarks/symex/bms/shuffle-5
+    benchmarks/symex/bms/sort-6
+    benchmarks/symex/bms/sort-7
+)
 
 python3 -u scripts/bench.py -o --output-dir "$1" \
-    --modes interpret-reorder interpret-unbatched compile-reorder compile-unbatched \
-    --ntrials 10 -j 40 --smt-solver-modes check-sat-assuming \
-    --benchmark-dirs benchmarks/symex/bms/* --record-work --small-tasks
+    --modes "${symex_modes[@]}" \
+    --ntrials $ntrials -j 40 --smt-solver-modes check-sat-assuming \
+    --record-work --small-tasks --timeout $timeout \
+    --benchmark-dirs "${symex_bms[@]}"
+
+## SCUBA
+
+scuba_modes=(
+    interpret
+    # interpret-unbatched
+    compile
+    # compile-unbatched
+    scuba
+)
+
+scuba_bms=(
+    benchmarks/scuba/bms/antlr
+    benchmarks/scuba/bms/avrora
+    benchmarks/scuba/bms/hedc
+    benchmarks/scuba/bms/hsqldb
+    benchmarks/scuba/bms/luindex
+    benchmarks/scuba/bms/polyglot
+    benchmarks/scuba/bms/sunflow
+    benchmarks/scuba/bms/toba-s
+    benchmarks/scuba/bms/weblech
+    benchmarks/scuba/bms/xalan
+)
 
 python3 -u scripts/bench.py -o --output-dir "$1" \
-    --modes interpret compile \
-    --ntrials 10 -j 40 --smt-solver-modes check-sat-assuming \
-    --benchmark-dirs benchmarks/scuba/bms/* --record-work
+    --modes "${scuba_modes[@]}" \
+    --ntrials $ntrials -j 40 --smt-solver-modes check-sat-assuming \
+    --record-work --timeout $timeout \
+    --benchmark-dirs "${scuba_bms[@]}"
